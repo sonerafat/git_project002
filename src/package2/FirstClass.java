0858b8596b1dfc0f56aa0b6dacce347f3c6c2e63
Soner Afat <sonerafat@gmail.com>
2020-08-09 18:07:05 +0200

    This iy the fifth commit from local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -7,7 +7,7 @@ public class FirstClass {
 		System.out.println("This is the first changes from local master");
 		System.out.println("This is second commit from local master");
 		System.out.println("This is the third commit from local master");
-		
+		System.out.println("This is first commit from remote master");
 		System.out.println("This is the fouth commit from local master");
 
 	}
