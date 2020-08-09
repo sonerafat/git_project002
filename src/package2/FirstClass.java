eceb6bd4ce6b5ad41b81ab03dee827bec9ce0ff5
Soner Afat <sonerafat@gmail.com>
2020-08-09 17:52:07 +0200

    forth commit from local

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -7,8 +7,8 @@ public class FirstClass {
 		System.out.println("This is the first changes from local master");
 		System.out.println("This is second commit from local master");
 		System.out.println("This is the third commit from local master");
-		System.out.println("This is the first commit from removed repo");
+		
+		System.out.println("This is the fouth commit from local master");
 
 	}
-
 }
